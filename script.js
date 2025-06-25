9f24a0485e4e8d406bf5c6f4a42919c757fba24d
Julian David Buitrago <jdborozco90@gmail.com>
2025-06-25 12:50:20 -0500

    ok CV

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 function downloadPDF() {
-    const element = document.getElementById('pdf-content');
+    const element = document.querySelector('.container-lg');
+
     const opt = {
-        margin: [1, 1, 3, 1],
-        filename: 'Hoja_de_vida_Julian_buitrago.pdf',
-        image: { type: 'jpeg', quality: 1 },
+        margin: [10, 5, 55 , 5],  // [arriba, izquierda, abajo, derecha] en mm
+        filename: 'Hoja_de_Vida_Julian_Buitrago.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
         html2canvas: {
-            scale: 2, useCors: true, scrolly: 0, windowWidth: document.body.scrollWidth,
-            windowHeight: document.body.scrollHeight
+            scale: 2,
+            useCORS: true,
+            allowTaint: false,
+            scrollY: 0
         },
-        jsPDF: { unit: 'mm', format: 'legal', orientation: 'portrait' },
-        pagebreak: { mode: ['css', 'legacy'] }
+        jsPDF: {
+            unit: 'mm',
+            format: 'a4',
+            orientation: 'portrait'
+        }
     };
+
     html2pdf().set(opt).from(element).save();
 }
