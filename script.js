5d19c86b38c0c85ce272e758729e48f3107db9b3
Julian David Buitrago <jdborozco90@gmail.com>
2025-09-17 16:47:07 -0500

    Actualizada Ok 17-09-2025

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 function downloadPDF() {
-    const element = document.querySelector('.container-lg');
+  const element = document.querySelector('#pdf-content');
 
-    const opt = {
-        margin: [10, 5, 20 , 5],  // [arriba, izquierda, abajo, derecha] en mm
-        filename: 'Hoja_de_Vida_Julian_Buitrago.pdf',
-        image: { type: 'jpeg', quality: 0.98 },
-        html2canvas: {
-            scale: 2,
-            useCORS: true,
-            allowTaint: false,
-            scrollY: 0
-        },
-        jsPDF: {
-            unit: 'mm',
-            format: 'a4',
-            orientation: 'portrait'
-        }
-    };
+  const opt = {
+    margin: [10, 5, 15, 5],  // [arriba, izquierda, abajo, derecha] en mm
+    filename: 'Hoja_de_Vida_Julian_Buitrago.pdf',
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: {
+      scale: 2,
+      useCORS: true,
+      allowTaint: false,
+      scrollY: 0
+    },
+    jsPDF: {
+      unit: 'mm',
+      format: 'a4',
+      orientation: 'portrait'
+    }
+  };
 
-    html2pdf().set(opt).from(element).save();
+  html2pdf().set(opt).from(element).save();
 }
